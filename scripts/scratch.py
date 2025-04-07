51f8650d5e6de57d91690f81f9d441a23e30396e
Edward Schrom <tec0@cdc.gov>
2025-04-07 13:23:27 -0400

    Function to calculate risk from parameters and titer

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -1,17 +1,16 @@
-# Scratchwork
-
 # %% Dependencies
 from typing import List
 
 import numpy as np
 
 
-# %% Function to make titer distribution
+# %% Function to simulate a titer distribution
 def simulate_titers(
     mns: List[float,], sds: List[float,], N: List[float,]
 ) -> np.ndarray:
     """
     Simulate antibody titers drawn from a (mixture of) lognormal distribution(s).
+    This is just to have some numbers to use, not necessarily to be realistic.
     """
     assert len(mns) == len(sds) & len(mns) == len(N), (
         "Means, standard deviations, and sample size must all be the same length."
@@ -29,8 +28,23 @@ def simulate_titers(
     return titer_samples
 
 
-# %% Make a titer distribution
+# %% Function to calculate risk from parameters and titers
+def calculate_risk(
+    midpoint: float,
+    steepness: float,
+    min_risk: float,
+    max_risk: float,
+    titer: float,
+):
+    return min_risk + (max_risk - min_risk) / (
+        1 + np.exp(steepness * (titer - midpoint))
+    )
 
+
+# %% Function to simulate a protection curve
+
+
+# %% Make a titer distribution
 titer_samples = simulate_titers(
     mns=[0.5, 1.0, 1.5, 2.0],
     sds=[0.25, 0.25, 0.25, 0.25],
