d9eeef1779acea3aa9ca7378f0a8d22e3ea8f1a5
Edward Schrom <tec0@cdc.gov>
2025-04-07 13:54:40 -0400

    Added seed for random numbers

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # %% Function to simulate a titer distribution
 def simulate_titers(
-    mns: List[float,], sds: List[float,], N: List[float,]
+    mns: List[float,], sds: List[float,], N: List[float,], seed: int
 ) -> np.ndarray:
     """
     Simulate antibody titers drawn from a (mixture of) lognormal distribution(s).
@@ -15,12 +15,13 @@ def simulate_titers(
     assert len(mns) == len(sds) & len(mns) == len(N), (
         "Means, standard deviations, and sample size must all be the same length."
     )
+    rng = np.random.default_rng(seed)
     titer_samples = np.array([])
     for i in range(len(mns)):
         titer_samples = np.concatenate(
             (
                 titer_samples,
-                10 ** np.random.normal(mns[i], sds[i], N[i]),
+                10 ** rng.normal(mns[i], sds[i], N[i]),
             ),
             axis=0,
         )
@@ -36,12 +37,43 @@ def calculate_risk(
     max_risk: float,
     titer: float,
 ):
+    """
+    Calculate risk from the 4 parameters of the double-scaled logit
+    and an antibody titer.
+    """
     return min_risk + (max_risk - min_risk) / (
         1 + np.exp(steepness * (titer - midpoint))
     )
 
 
-# %% Function to simulate a protection curve
+# %% Function to calculate protection from risk
+def calculate_protection(risk_x: float, risk_0: float):
+    """
+    Calculate protection using the odds ratio definition,
+    using titer = 0 as the baseline for comparison.
+    """
+    return 1 - (risk_x / (1 - risk_x) / (risk_0 / (1 - risk_0)))
+
+
+# %% Function to simulate parameters of a risk curve
+def simulate_risk_parameters(
+    midpoint_mn: float,
+    midpoint_sd: float,
+    steepness_mn: float,
+    steepness_sd: float,
+    min_risk_mn: float,
+    min_risk_sd: float,
+    max_risk_mn: float,
+    max_risk_sd: float,
+    N: int,
+    seed: int,
+):
+    """
+    Simulate N draws from a posterior distribution of fit parameters
+    for a double-scaled logit risk curve
+    """
+
+    return None
 
 
 # %% Make a titer distribution
@@ -49,4 +81,5 @@ titer_samples = simulate_titers(
     mns=[0.5, 1.0, 1.5, 2.0],
     sds=[0.25, 0.25, 0.25, 0.25],
     N=[100, 100, 100, 100],
+    seed=1,
 )
