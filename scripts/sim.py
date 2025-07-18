b35dc0a2fd316484a3355d0a6bb7a88b75bf8854
Edward Schrom <67654180+eschrom@users.noreply.github.com>
2025-07-18 11:12:40 -0400

    Rudimentary sampling schemes (#14)
    * X sec survey and simpler numerical sampling in sim
    
    * Fixed cross sectional sampling
    
    * TND sampling
    
    * Enforce a time window on the cross sectional survey
    
    * Fixed a few labels
    
    * Swapped order of some sim steps
    
    * Plotted sampled vs real titer distribution
    
    * Plotted sampled vs real titer distribution
    
    * Rearranged code

--- scripts/sim.py
+++ scripts/sim.py
@@ -1,10 +1,15 @@
 # %% Import modules
+import sys
+
 import altair as alt
 import numpy as np
 import polars as pl
 
+sys.path.append("/home/tec0/cfa-sero-protection")
+import seropro.samples as sps
+
 # %% Define parameters
-POP_SIZE = 1000
+POP_SIZE = 10000
 NUM_DAYS = 100
 AB_RISK_SLOPE = 0.02
 AB_RISK_MIDPOINT = 500.0
@@ -25,6 +30,10 @@ TC_LAG = 4
 FORCE_EXP = 0.1
 FORCE_VAX = 0.02
 RECOVERY = 0.25
+XSEC_SIZE = 100
+XSEC_WINDOW = [50, 57]
+TND_INF_PRB = 0.1
+TND_NON_PRB = 0.01
 
 
 # %% Define functions
@@ -55,8 +64,8 @@ for i in range(NUM_DAYS):
                 "id": range(0, POP_SIZE),
                 "inf_status": [False] * POP_SIZE,
                 "vax_status": [False] * POP_SIZE,
-                "ab": [0] * POP_SIZE,
-                "tc": [0] * POP_SIZE,
+                "ab": [0.0] * POP_SIZE,
+                "tc": [0.0] * POP_SIZE,
                 "inf_new_lag_ab": [False] * POP_SIZE,
                 "vax_new_lag_ab": [False] * POP_SIZE,
                 "inf_new_lag_tc": [False] * POP_SIZE,
@@ -64,23 +73,40 @@ for i in range(NUM_DAYS):
             }
         )
     else:
-        new_daily_data = daily_data[i - 1].with_columns(
-            ab=pl.col("ab")
-            * pl.Series(
-                "ab",
-                (np.random.rand(POP_SIZE) * (AB_DECAY[1] - AB_DECAY[0]))
-                + AB_DECAY[0],
-            ),
-            tc=pl.col("tc")
-            * pl.Series(
-                "tc",
-                (np.random.rand(POP_SIZE) * (TC_DECAY[1] - TC_DECAY[0]))
-                + TC_DECAY[0],
-            ),
-            inf_new_lag_ab=daily_data[max(0, i - AB_LAG)]["inf_new"],
-            vax_new_lag_ab=daily_data[max(0, i - AB_LAG)]["vax_new"],
-            inf_new_lag_tc=daily_data[max(0, i - TC_LAG)]["inf_new"],
-            vax_new_lag_tc=daily_data[max(0, i - TC_LAG)]["vax_new"],
+        new_daily_data = (
+            daily_data[i - 1]
+            .with_columns(
+                ab=pl.col("ab")
+                * pl.Series(
+                    "ab", np.random.uniform(AB_DECAY[0], AB_DECAY[1], POP_SIZE)
+                ),
+                tc=pl.col("tc")
+                * pl.Series(
+                    "tc", np.random.uniform(TC_DECAY[0], TC_DECAY[1], POP_SIZE)
+                ),
+                inf_new_lag_ab=daily_data[max(0, i - AB_LAG)]["inf_new"],
+                vax_new_lag_ab=daily_data[max(0, i - AB_LAG)]["vax_new"],
+                inf_new_lag_tc=daily_data[max(0, i - TC_LAG)]["inf_new"],
+                vax_new_lag_tc=daily_data[max(0, i - TC_LAG)]["vax_new"],
+            )
+            .with_columns(
+                ab=pl.when(pl.col("inf_new_lag_ab") | pl.col("vax_new_lag_ab"))
+                .then(
+                    pl.Series(
+                        "ab",
+                        np.random.uniform(AB_SPIKE[0], AB_SPIKE[1], POP_SIZE),
+                    )
+                )
+                .otherwise(pl.col("ab")),
+                tc=pl.when(pl.col("inf_new_lag_tc") | pl.col("vax_new_lag_tc"))
+                .then(
+                    pl.Series(
+                        "tc",
+                        np.random.uniform(TC_SPIKE[0], TC_SPIKE[1], POP_SIZE),
+                    )
+                )
+                .otherwise(pl.col("tc")),
+            )
         )
 
     new_daily_data = (
@@ -129,26 +155,6 @@ for i in range(NUM_DAYS):
         )
         .with_columns(vax_status=pl.col("vax_status") | pl.col("vax_new"))
         .with_columns(
-            ab=pl.when(pl.col("inf_new_lag_ab") | pl.col("vax_new_lag_ab"))
-            .then(
-                pl.Series(
-                    "ab",
-                    (np.random.rand(POP_SIZE) * (AB_SPIKE[1] - AB_SPIKE[0]))
-                    + AB_SPIKE[0],
-                )
-            )
-            .otherwise(pl.col("ab")),
-            tc=pl.when(pl.col("inf_new_lag_tc") | pl.col("vax_new_lag_tc"))
-            .then(
-                pl.Series(
-                    "tc",
-                    (np.random.rand(POP_SIZE) * (TC_SPIKE[1] - TC_SPIKE[0]))
-                    + TC_SPIKE[0],
-                )
-            )
-            .otherwise(pl.col("tc")),
-        )
-        .with_columns(
             rec_new=pl.when((pl.col("rec_draw") < 1.0) & ~pl.col("inf_new"))
             .then(True)
             .otherwise(False)
@@ -158,27 +164,32 @@ for i in range(NUM_DAYS):
             .then(False)
             .otherwise(pl.col("inf_status"))
         )
-        # .select("id", "day", "inf_status", "vax_status", "ab", "tc")
     )
 
     daily_data.append(new_daily_data)
 
 all_data = pl.concat(daily_data).select(
-    "id", "day", "inf_status", "vax_status", "ab", "tc"
+    "id",
+    "day",
+    "inf_status",
+    "inf_new",
+    "vax_status",
+    "ab",
+    "tc",
 )
 
 # %% Plot number of people currently infected through time
 inf_plot = all_data.group_by("day").agg(pl.col("inf_status").sum())
 alt.Chart(inf_plot).mark_line().encode(
     x=alt.X("day:Q", title="Day"),
-    y=alt.Y("inf_status:Q", title="Number Infected"),
+    y=alt.Y("inf_status:Q", title="Current Number Infected"),
 )
 
 # %% Plot number of people cumulatively vaccinated through time
 vax_plot = all_data.group_by("day").agg(pl.col("vax_status").sum())
 alt.Chart(vax_plot).mark_line().encode(
     x=alt.X("day:Q", title="Day"),
-    y=alt.Y("vax_status:Q", title="Number Infected"),
+    y=alt.Y("vax_status:Q", title="Cumulative Number Vaccinated"),
 )
 
 # %% Plot the antibody protection curve
@@ -191,3 +202,62 @@ alt.Chart(pro_plot).mark_line().encode(
     x=alt.X("ab:Q", title="Antibody Titer"),
     y=alt.Y("risk:Q", title="Risk", scale=alt.Scale(domain=[0, 1])),
 )
+
+# %% Collect a cross sectional serosurvey
+xsec_samples = pl.DataFrame(
+    {
+        "id": np.random.choice(POP_SIZE, XSEC_SIZE, replace=False),
+        "day": np.random.choice(
+            np.arange(XSEC_WINDOW[0], XSEC_WINDOW[1]), XSEC_SIZE
+        ),
+    }
+)
+
+xsec_data = (
+    all_data.join(xsec_samples, ["id", "day"], "semi")
+    .select(["id", "ab"])
+    .rename({"id": "pop_id", "ab": "titer"})
+    .with_columns(pl.col("pop_id").cast(pl.UInt32), day=pl.lit("50-56"))
+)
+
+xsec_real = (
+    all_data.filter((pl.col("day") >= 50) & (pl.col("day") <= 56))
+    .select(["id", "ab", "day"])
+    .rename({"id": "pop_id", "ab": "titer"})
+    .with_columns(
+        pl.col("pop_id").cast(pl.UInt32), pl.col("day").cast(pl.String)
+    )
+)
+
+# %% Conduct a TND serosurvey
+tnd_inf_samples = all_data.filter(pl.col("inf_new")).sample(
+    fraction=TND_INF_PRB
+)
+tnd_non_samples = all_data.filter(~pl.col("inf_status")).sample(
+    fraction=TND_NON_PRB
+)
+tnd_data = pl.concat([tnd_inf_samples, tnd_non_samples])
+
+# %% Plot serosurvey titer distribution vs. true values
+xsec_data_dens = sps.TiterSamples(
+    xsec_data,
+    pl.DataFrame({"titer": [0.0, 1000.0]}).with_row_index("pop_id"),
+).to_density(values="titer", groups="day")
+
+xsec_real_dens = sps.TiterSamples(
+    xsec_real,
+    pl.DataFrame({"titer": [0.0, 1000.0]}).with_row_index("pop_id"),
+).to_density(values="titer", groups="day")
+
+alt.data_transformers.disable_max_rows()
+output = alt.Chart(xsec_real_dens).mark_line(
+    opacity=0.1, color="green"
+).encode(
+    x=alt.X("titer:Q", title="Titer"),
+    y=alt.Y("density:Q", title="Density"),
+    detail="day",
+) + alt.Chart(xsec_data_dens).mark_line(opacity=1.0, color="black").encode(
+    x=alt.X("titer:Q"),
+    y=alt.Y("density:Q"),
+)
+output.display()
