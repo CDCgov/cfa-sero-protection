1654c7a0b626355d66a34f579f2831b1b6cd2832
Edward Schrom <tec0@cdc.gov>
2025-08-07 14:38:13 -0400

    Adapted and confirmed more complex simulation

--- scripts/sim.py
+++ scripts/sim.py
@@ -376,168 +376,25 @@ plot = (
 )
 plot.display()
 
-# %% Recreate Casey's simulation. #################################################
-POP_SIZE = 10000
-CASES = 1000
-CONTROLS = 4000
-MAX_TITER = 5.0
-BIAS = 0.0
-NOISE = 0.0
-RISK_SLOPE = 2
-RISK_MIDPOINT = 2.5
-RISK_MAX = 1.0
-
-pop = (
-    pl.DataFrame(
-        {
-            "id": range(0, POP_SIZE),
-            "titer": np.random.uniform(0, MAX_TITER, POP_SIZE),
-            "draw": np.random.uniform(0, 1, POP_SIZE),
-        }
-    )
-    .with_columns(
-        other=(
-            pl.col("titer")
-            + pl.Series(np.random.normal(BIAS, NOISE, POP_SIZE))
-        ).clip(lower_bound=0.0)
-    )
-    .with_columns(
-        titer_risk=scaled_logit_risk(
-            pl.col("titer"), RISK_SLOPE, RISK_MIDPOINT, RISK_MAX
-        ),
-        other_risk=scaled_logit_risk(
-            pl.col("other"), RISK_SLOPE, RISK_MIDPOINT, RISK_MAX
-        ),
-    )
-    .with_columns(risk=pl.min_horizontal("titer_risk", "other_risk"))
-    .with_columns(
-        sick=pl.when(pl.col("draw") < pl.col("risk"))
-        .then(pl.lit(True))
-        .otherwise(pl.lit(False))
-    )
-)
-
-controls = pop.sample(CONTROLS).with_columns(sick=pl.lit(False))
-cases = (
-    pop.join(controls, on="id", how="anti")
-    .filter(pl.col("sick"))
-    .sample(CASES)
-)
-tnd_data = pl.concat([cases, controls])
-
-
-# %% Build a numpyro model of protection
-def scaled_logit_model(
-    titer,
-    sick,
-    slope_shape=2.0,
-    slope_rate=2.0,
-    midpoint_shape=2.5,
-    midpoint_rate=1.5,
-    max_risk_shape1=5.0,
-    max_risk_shape2=1.0,
-):
-    slope = numpyro.sample("slope", dist.Gamma(slope_shape, slope_rate))
-    midpoint = numpyro.sample(
-        "midpoint", dist.Gamma(midpoint_shape, midpoint_rate)
-    )
-    max_risk = numpyro.sample(
-        "max_risk", dist.Beta(max_risk_shape1, max_risk_shape2)
-    )
-    mu = max_risk / (1 + jnp.exp(slope * (titer - midpoint)))
-    numpyro.sample("obs", dist.Binomial(probs=mu), obs=sick)
-
-
-# %% Fit the numpyro model of protection
-NUM_SAMPLES = 200
-titer = tnd_data["titer"].to_numpy()
-sick = tnd_data["sick"].to_numpy() * 1
-kernel = NUTS(scaled_logit_model, init_strategy=init_to_sample)
-mcmc = MCMC(kernel, num_warmup=1000, num_samples=NUM_SAMPLES, num_chains=4)
-mcmc.run(random.key(0), titer=titer, sick=sick)
-mcmc.print_summary()
-
-# %% Plot the true protection curve vs. the inferred curve
-prot_real = (
-    pl.DataFrame({"titer": np.arange(0, MAX_TITER + 0.01, 0.01)})
-    .with_columns(
-        risk=scaled_logit_risk(
-            pl.col("titer"),
-            RISK_SLOPE,
-            RISK_MIDPOINT,
-            RISK_MAX,
-        )
-    )
-    .with_columns(protection=odds_ratio_protection(pl.col("risk")))
-)
-mcmc_samples = mcmc.get_samples()
-prot = []
-for i in range(NUM_SAMPLES):
-    new_prot_infer = (
-        pl.DataFrame({"titer": np.arange(0, MAX_TITER + 0.01, 0.01)})
-        .with_columns(
-            risk=scaled_logit_risk(
-                pl.col("titer"),
-                mcmc_samples["slope"][i],
-                mcmc_samples["midpoint"][i],
-                mcmc_samples["max_risk"][i],
-            ),
-            sample_id=pl.lit(i),
-        )
-        .with_columns(protection=odds_ratio_protection(pl.col("risk")))
-    )
-    prot.append(new_prot_infer)
-prot_infer = pl.concat(prot)
-
-# %% Plot inferred risk vs. real
-alt.data_transformers.disable_max_rows()
-output = alt.Chart(prot_infer).mark_line(opacity=0.01, color="black").encode(
-    x=alt.X("titer:Q", title="Antibody Titer"),
-    y=alt.Y("risk:Q", title="Risk", scale=alt.Scale(domain=[0, 1])),
-    detail="sample_id",
-) + alt.Chart(prot_real).mark_line(opacity=1.0, color="green").encode(
-    x=alt.X("titer:Q", title="Antibody Titer"),
-    y=alt.Y("risk:Q", title="Risk", scale=alt.Scale(domain=[0, 1])),
-)
-output.display()
-
-# %% Plot inferred protection vs. real
-alt.data_transformers.disable_max_rows()
-output = alt.Chart(prot_infer).mark_line(opacity=0.01, color="black").encode(
-    x=alt.X("titer:Q", title="Antibody Titer"),
-    y=alt.Y(
-        "protection:Q", title="Protection", scale=alt.Scale(domain=[0, 1])
-    ),
-    detail="sample_id",
-) + alt.Chart(prot_real).mark_line(opacity=1.0, color="green").encode(
-    x=alt.X("titer:Q", title="Antibody Titer"),
-    y=alt.Y(
-        "protection:Q", title="Protection", scale=alt.Scale(domain=[0, 1])
-    ),
-)
-output.display()
-
 # %% EVERYTHING BELOW IS SCRATCHWORK FOR MORE COMPLICATED STUFF
 
 # %% Define parameters
 POP_SIZE = 10000
 NUM_DAYS = 100
-AB_RISK_SLOPE = 0.02
-AB_RISK_MIDPOINT = 500.0
-AB_RISK_MIN = 0.1
+AB_RISK_SLOPE = 2
+AB_RISK_MIDPOINT = 6
 AB_RISK_MAX = 0.9
 AB_RISK_MIDPOINT_DROP = 0.0
 AB_DECAY = [0.9, 1.0]
-AB_SPIKE = [500.0, 1000.0]
-AB_LAG = 4
-TC_RISK_SLOPE = 0.02
-TC_RISK_MIDPOINT = 500.0
-TC_RISK_MIN = 0.1
-TC_RISK_MAX = 0.9
-TC_RISK_MIDPOINT_DROP = 0.0
-TC_DECAY = [0.9, 1.0]
-TC_SPIKE = [500.0, 1000.0]
-TC_LAG = 4
+AB_SPIKE = [8, 10]
+NONAB_BIAS = 0
+NONAB_NOISE = 0
+NONAB_RISK_SLOPE = 2
+NONAB_RISK_MIDPOINT = 6
+NONAB_RISK_MAX = 0.9
+NONAB_RISK_MIDPOINT_DROP = 0.0
+NONAB_DECAY = [0.9, 1.0]
+LAG = 4
 FORCE_EXP = 0.1
 FORCE_VAX = 0.02
 RECOVERY = 0.25
@@ -552,20 +409,17 @@ def calculate_risk(
     titer: pl.Expr,
     slope: float,
     midpoint: float,
-    min_risk: float,
     max_risk: float,
 ) -> pl.Expr:
     """
     Define risk as a function of log antibody titer,
-    following a double-scaled logit functional form.
+    following a scaled logit functional form.
     """
 
-    return min_risk + (max_risk - min_risk) / (
-        1 + (slope * (titer - midpoint)).exp()
-    )
+    return max_risk / (1 + (slope * (titer - midpoint)).exp())
 
 
-def calculate_protection(risk: pl.Expr) -> pl.Expr:
+def calculate_oddsratio(risk: pl.Expr) -> pl.Expr:
     """
     Define protection as one minus the odds ratio.
     """
@@ -583,11 +437,9 @@ for i in range(NUM_DAYS):
                 "inf_status": [False] * POP_SIZE,
                 "vax_status": [False] * POP_SIZE,
                 "ab": [0.0] * POP_SIZE,
-                "tc": [0.0] * POP_SIZE,
-                "inf_new_lag_ab": [False] * POP_SIZE,
-                "vax_new_lag_ab": [False] * POP_SIZE,
-                "inf_new_lag_tc": [False] * POP_SIZE,
-                "vax_new_lag_tc": [False] * POP_SIZE,
+                "nonab": [0.0] * POP_SIZE,
+                "inf_new_lag": [False] * POP_SIZE,
+                "vax_new_lag": [False] * POP_SIZE,
             }
         )
     else:
@@ -597,18 +449,20 @@ for i in range(NUM_DAYS):
                 ab=pl.col("ab")
                 * pl.Series(
                     "ab", np.random.uniform(AB_DECAY[0], AB_DECAY[1], POP_SIZE)
-                ),
-                tc=pl.col("tc")
-                * pl.Series(
-                    "tc", np.random.uniform(TC_DECAY[0], TC_DECAY[1], POP_SIZE)
-                ),
-                inf_new_lag_ab=daily_data[max(0, i - AB_LAG)]["inf_new"],
-                vax_new_lag_ab=daily_data[max(0, i - AB_LAG)]["vax_new"],
-                inf_new_lag_tc=daily_data[max(0, i - TC_LAG)]["inf_new"],
-                vax_new_lag_tc=daily_data[max(0, i - TC_LAG)]["vax_new"],
+                )
             )
             .with_columns(
-                ab=pl.when(pl.col("inf_new_lag_ab") | pl.col("vax_new_lag_ab"))
+                nonab=pl.col("ab")
+                + pl.Series(
+                    np.random.normal(NONAB_BIAS, NONAB_NOISE, POP_SIZE)
+                ).clip(lower_bound=0.0)
+            )
+            .with_columns(
+                inf_new_lag=daily_data[max(0, i - LAG)]["inf_new"],
+                vax_new_lag=daily_data[max(0, i - LAG)]["vax_new"],
+            )
+            .with_columns(
+                ab=pl.when(pl.col("inf_new_lag") | pl.col("vax_new_lag"))
                 .then(
                     pl.Series(
                         "ab",
@@ -616,14 +470,14 @@ for i in range(NUM_DAYS):
                     )
                 )
                 .otherwise(pl.col("ab")),
-                tc=pl.when(pl.col("inf_new_lag_tc") | pl.col("vax_new_lag_tc"))
+                nonab=pl.when(pl.col("inf_new_lag") | pl.col("vax_new_lag"))
                 .then(
                     pl.Series(
-                        "tc",
-                        np.random.uniform(TC_SPIKE[0], TC_SPIKE[1], POP_SIZE),
-                    )
+                        "nonab",
+                        np.random.normal(NONAB_BIAS, NONAB_NOISE, POP_SIZE),
+                    ).clip(lower_bound=0.0)
                 )
-                .otherwise(pl.col("tc")),
+                .otherwise(pl.col("nonab")),
             )
         )
 
@@ -633,15 +487,13 @@ for i in range(NUM_DAYS):
                 pl.col("ab"),
                 AB_RISK_SLOPE,
                 AB_RISK_MIDPOINT - (AB_RISK_MIDPOINT_DROP * i),
-                AB_RISK_MIN,
                 AB_RISK_MAX,
             ),
-            tc_risk=calculate_risk(
-                pl.col("tc"),
-                TC_RISK_SLOPE,
-                TC_RISK_MIDPOINT - (TC_RISK_MIDPOINT_DROP * i),
-                TC_RISK_MIN,
-                TC_RISK_MAX,
+            nonab_risk=calculate_risk(
+                pl.col("nonab"),
+                NONAB_RISK_SLOPE,
+                NONAB_RISK_MIDPOINT - (NONAB_RISK_MIDPOINT_DROP * i),
+                NONAB_RISK_MAX,
             ),
             inf_draw=pl.Series("inf_draw", np.random.rand(POP_SIZE))
             / FORCE_EXP,
@@ -653,7 +505,10 @@ for i in range(NUM_DAYS):
         )
         .with_columns(
             inf_new=pl.when(
-                (pl.col("inf_draw") < pl.min_horizontal("ab_risk", "tc_risk"))
+                (
+                    pl.col("inf_draw")
+                    < pl.min_horizontal("ab_risk", "nonab_risk")
+                )
                 & ~pl.col("inf_status")
             )
             .then(True)
@@ -693,7 +548,7 @@ all_data = pl.concat(daily_data).select(
     "inf_new",
     "vax_status",
     "ab",
-    "tc",
+    "nonab",
 )
 
 # %% Plot number of people currently infected through time
